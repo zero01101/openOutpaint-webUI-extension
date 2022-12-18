6a3cc9a89fa1457f2256b97dcb117fbdcb83bc56
zero01101 <timh@americanct.com>
2022-12-18 14:29:53 -0600

    i swear it worked before

--- install.py
+++ install.py
@@ -1,10 +1,10 @@
 import launch
+import os
 from modules import scripts
 from pathlib import Path
 
 base_location = Path(__file__).parent
-repo_destination = Path.joinpath(base_location, "app")
+repo_destination = os.path.join(base_location, "app")
 name = "openOutpaint"
 repo = "https://github.com/zero01101/openOutpaint"
-print("cloning openOutpaint repo into " + repo_destination)
 launch.git_clone(repo, repo_destination, "openOutpaint")
