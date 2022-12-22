7201f497c222eb7320f679cb2019a98cdabca046
zero01101 <timh@americanct.com>
2022-12-22 16:26:12 -0600

    Revert "install submodules in install.py"
    This reverts commit b819576e3033966a0ff79f8494842146e1f84d05.

--- install.py
+++
@@ -1,8 +0,0 @@
-import os
-from launch import run
-from modules import scripts
-
-git = os.environ.get('GIT', "git")
-usefulDirs = scripts.basedir().split(os.sep)[-2:]
-print("preinit openOutpaint")
-print(run(f'"{git}" -C ' + scripts.basedir() + ' submodule update --init --recursive --remote'))
--- scripts/main.py
+++ scripts/main.py
@@ -37,10 +37,10 @@ def add_tab():
 usefulDirs = scripts.basedir().split(os.sep)[-2:]
 
 print("openOutpaint init")
-#print(scripts.basedir())
-#print(usefulDirs)
+print(scripts.basedir())
+print(usefulDirs)
 git = os.environ.get('GIT', "git")
-#print(git)
+print(git)
 print(run(f'"{git}" -C ' + scripts.basedir() +
       ' submodule update --init --recursive --remote'))
 
