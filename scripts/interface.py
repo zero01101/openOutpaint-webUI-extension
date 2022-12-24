1e96785661bdaeae24a2d831c1c172b83c1c44f2
Victor Seiji Hariki <victorseijih@gmail.com>
2022-12-23 21:56:21 -0300

    removes unecessary things from interface.py
    it's also better to use fit-content instead of max-width in this case.
    
    Signed-off-by: Victor Seiji Hariki <victorseijih@gmail.com>

--- scripts/interface.py
+++ scripts/interface.py
@@ -1,13 +1,8 @@
 import gradio as gr
-from modules import script_callbacks, shared, scripts
-
-elements = {}
+from modules import scripts
 
 
 class Script(scripts.Script):
-    img2img = None
-    txt2img = None
-
     def title(self):
         return "OpenOutpaint"
 
@@ -15,19 +10,12 @@ class Script(scripts.Script):
         return scripts.AlwaysVisible
 
     def after_component(self, component, **kwargs):
-        global elements, share_image
-        if kwargs.get('elem_id') is not None:
-            if kwargs.get('elem_id').find('txt2img_gallery') != -1:
-                self.txt2img = component
-            elif kwargs.get('elem_id').find('img2img_gallery') != -1:
-                self.img2img = component
+        # Add button to both txt2img and img2img tabs
         if kwargs.get("value") == "Send to extras":
-            elements["openoutpaint_button"] = gr.Button(
+            basic_send_button = gr.Button(
                 "Send to openOutpaint", elem_id=f"openoutpaint_button")
-            elements["openoutpaint_button"].click(None, [], None,
-                                                  _js="() => openoutpaint_send_gallery()")
+            basic_send_button.click(None, [], None,
+                                    _js="() => openoutpaint_send_gallery('WebUI Resource')")
 
     def ui(self, is_img2img):
-        if elements.get("openoutpaint_button"):
-            pass
         return []
