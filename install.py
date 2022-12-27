d793f39ece92e3fd5fdcf6efa819d1278ac609fc
Victor Seiji Hariki <victorseijih@gmail.com>
2022-12-26 22:05:17 -0300

    Test fix for #11
    Updates app reference
    
    Signed-off-by: Victor Seiji Hariki <victorseijih@gmail.com>

--- install.py
+++ install.py
@@ -1,10 +1,21 @@
 import sys
 import os
-from launch import run
 from modules import scripts
 
 git = os.environ.get('GIT', "git")
 usefulDirs = sys.argv[0].split(os.sep)[-3:]
 print("preinit openOutpaint")
-installDir = scripts.basedir() + os.sep + usefulDirs[0] + os.sep + usefulDirs[1]
-print(run(f'"{git}" -C "' + installDir + '" submodule update --init --recursive --remote'))
+
+installDir = scripts.basedir() + os.sep + \
+    usefulDirs[0] + os.sep + usefulDirs[1]
+
+# Attempt to use launch module from webui
+command = f'"{git}" -C "' + installDir +\
+    '" submodule update --init --recursive --remote'
+try:
+    from launch import run
+
+    print(run(command))
+except ImportError:
+    print("[openoutpaint-extension] We failed to import the 'launch' module. Using 'os'")
+    os.system(command)
